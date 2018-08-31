98be3f4ecef7dee1aa16d3861445dc7bf493db18
Anh <anhvietle.info@gmail.com>
2018-09-01 01:11:50 +0800

    settings fix

--- settings.py
+++ settings.py
@@ -8,12 +8,12 @@ if getattr(sys, 'frozen', False):
 elif __file__:
     name = __file__
     TEST_MODE = True
-APPLICATION_PATH = os.path.dirname(__file__)
+APPLICATION_PATH = os.path.dirname(name)
 APPLICATION_FILE_PATH = os.path.join(
-    APPLICATION_PATH, os.path.basename(__file__))
+    APPLICATION_PATH, os.path.basename(name))
 
 # provide a file for test mode instead of recursively encrypting everything
-ENCRYPT_FOLDER_PATH = "test_folder" if TEST_MODE else "asdf"
+ENCRYPT_FOLDER_PATH = "test_folder" if TEST_MODE else APPLICATION_PATH
 
 STATUS_FILE_PATH = os.path.join(APPLICATION_PATH, ".lockbox_status.json")
 LOG_FILE_PATH = os.path.join(APPLICATION_PATH, ".lockbox_logs")
